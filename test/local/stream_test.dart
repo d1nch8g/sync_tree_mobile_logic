e229ad8ab46888e7295ddd26108e804ed8276e9a
Danila Fomin <dangdancheg@gmail.com>
2021-09-12 16:18:39 +0300

    .

--- test/local/stream_test.dart
+++ test/local/stream_test.dart
@@ -1,5 +1,14 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:sync_tree_dart_crypt/sync_tree_dart_crypt.dart';
 
 void main() {
-  test('stream create value', () {});
+  test('stream trigger event', () {
+    triggerEvent(trigger: Trigger.mainBalanceUpdate);
+  });
+  test('stream create listener function', () {
+    var doSomething = () {
+      var x = 1 + 3;
+      return;
+    };
+  });
 }
