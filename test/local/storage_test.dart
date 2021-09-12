f3302628f1b69562a70f0fc9589dd39d736131b3
Danila Fomin <dangdancheg@gmail.com>
2021-09-12 16:08:24 +0300

    .

--- test/local/storage_test.dart
+++ test/local/storage_test.dart
@@ -3,17 +3,17 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sync_tree_dart_crypt/sync_tree_dart_crypt.dart';
 
 void main() {
-  test('storage save value test', () async {
+  test('storage save value', () async {
     SharedPreferences.setMockInitialValues({});
     saveValue(StorageKey.connectedWallets, 'value');
   });
-  test('storage load value test', () async {
+  test('storage load value', () async {
     SharedPreferences.setMockInitialValues({
       'connectedWallets': 'value',
     });
     var loaded = await loadValue(StorageKey.connectedWallets);
     if (loaded != 'value') {
-      fail('value loaded from prefs is not matching $loaded');
+      fail('value loaded from prefs is not matching');
     }
   });
 }
