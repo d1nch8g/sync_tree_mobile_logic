831fbcde22c458031bf3f32c54518092e100ca6a
Danila Fomin <dangdancheg@gmail.com>
2021-09-12 16:05:35 +0300

    added tests for storage module

--- test/local/storage_test.dart
+++ test/local/storage_test.dart
@@ -1,3 +1,19 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sync_tree_dart_crypt/sync_tree_dart_crypt.dart';
 
-void main() {}
+void main() {
+  test('storage save value test', () async {
+    SharedPreferences.setMockInitialValues({});
+    saveValue(StorageKey.connectedWallets, 'value');
+  });
+  test('storage load value test', () async {
+    SharedPreferences.setMockInitialValues({
+      'connectedWallets': 'value',
+    });
+    var loaded = await loadValue(StorageKey.connectedWallets);
+    if (loaded != 'value') {
+      fail('value loaded from prefs is not matching $loaded');
+    }
+  });
+}
