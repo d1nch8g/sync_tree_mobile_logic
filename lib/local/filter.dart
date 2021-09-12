debd4c4a89861831519c17a1d1b39984b90d97d7
Danila Fomin <dangdancheg@gmail.com>
2021-09-12 14:15:48 +0300

    removed all ui components from operation a filter text checkout module

--- lib/local/filter.dart
+++ lib/local/filter.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/material.dart';
-
 class Filter {
   bool isProfane(String stringToTest) {
     final lowerCaseStringToTest = stringToTest.toLowerCase();
@@ -44,107 +42,21 @@ class Filter {
     return false;
   }
 
-  bool operateCheck(TextEditingController nameController, context) {
-    if (isProfane(nameController.text)) {
-      nameController.text = '';
-      showDialog(
-        context: context,
-        builder: (_) => BadNameOverlay(
-          mainText: 'name contains profane words',
-        ),
-      );
-      return false;
+  /// checks string wether it contains any bad words
+  String operateCheck(text) {
+    if (isProfane(text)) {
+      return 'name contains profane words';
     }
-    if (isShort(nameController.text)) {
-      nameController.text = '';
-      showDialog(
-        context: context,
-        builder: (_) => BadNameOverlay(
-          mainText: 'name is too short',
-        ),
-      );
-      return false;
+    if (isShort(text)) {
+      return 'name is too short';
     }
-    if (containBadCharacters(nameController.text)) {
-      nameController.text = '';
-      showDialog(
-        context: context,
-        builder: (_) => BadNameOverlay(
-          mainText: 'name contains bad letters',
-        ),
-      );
-      return false;
+    if (containBadCharacters(text)) {
+      return 'name contains bad letters';
     }
-    if (tooLong(nameController.text)) {
-      nameController.text = '';
-      showDialog(
-        context: context,
-        builder: (_) => BadNameOverlay(
-          mainText: 'name is too long',
-        ),
-      );
-      return false;
+    if (tooLong(text)) {
+      return 'name is too long';
     }
-    return true;
-  }
-}
-
-class BadNameOverlay extends StatefulWidget {
-  final String mainText;
-  BadNameOverlay({this.mainText = 'error'});
-  @override
-  State<StatefulWidget> createState() => BadNameOverlayState();
-}
-
-class BadNameOverlayState extends State<BadNameOverlay>
-    with SingleTickerProviderStateMixin {
-  late AnimationController controller;
-  late Animation<double> scaleAnimation;
-
-  @override
-  void initState() {
-    super.initState();
-    controller = AnimationController(
-      vsync: this,
-      duration: Duration(milliseconds: 377),
-    );
-    scaleAnimation = CurvedAnimation(
-      parent: controller,
-      curve: Curves.decelerate,
-    );
-    controller.addListener(() {
-      setState(() {});
-    });
-    controller.forward();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Material(
-        color: Colors.transparent,
-        child: ScaleTransition(
-          scale: scaleAnimation,
-          child: Container(
-            width: MediaQuery.of(context).size.width * 0.8,
-            decoration: ShapeDecoration(
-              color: Theme.of(context).backgroundColor,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(50.0),
-              child: Text(
-                this.widget.mainText,
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.headline2,
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
+    return '';
   }
 }
 
