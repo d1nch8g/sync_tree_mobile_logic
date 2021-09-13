75fbec89d8de19438fb2b7cea2d0bf0a560a97ed
Danila Fomin <dangdancheg@gmail.com>
2021-09-13 12:24:02 +0300

    moved stream logic to storage module, because it always refers to storage events

--- lib/local/data.dart
+++ lib/local/data.dart
@@ -73,14 +73,46 @@ class Storage {
     var prefs = await SharedPreferences.getInstance();
     return prefs.getInt('balance') ?? 0;
   }
-  
+
+  void saveConnectedWalletsAdressesList(List<String> wallets) async {
+    var prefs = await SharedPreferences.getInstance();
+    prefs.setStringList('wallets', wallets);
+  }
+
+  Future<List<String>> loadConnectedWallets() async {
+    var prefs = await SharedPreferences.getInstance();
+    return prefs.getStringList('wallets') ?? [];
+  }
+
+  void saveMarketBalanceByAdress(String adress, int balance) async {
+    var prefs = await SharedPreferences.getInstance();
+    prefs.setInt(adress, balance);
+    triggerEvent(trigger: Trigger.marketBalanceUpdate);
+  }
+
+  Future<int> loadMarketBalance(String adress) async {
+    var prefs = await SharedPreferences.getInstance();
+    return prefs.getInt(adress) ?? 0;
+  }
+
+  void savePublicName(String name) async {
+    var prefs = await SharedPreferences.getInstance();
+    prefs.setString('name', name);
+    triggerEvent(trigger: Trigger.publicNameUpdate);
+  }
+
+  Future<String> loadPublicName() async {
+    var prefs = await SharedPreferences.getInstance();
+    return prefs.getString('name') ?? 'name_err';
+  }
+
+  void saveSearchCache(String cache) async {
+    var prefs = await SharedPreferences.getInstance();
+    prefs.setString('cache', cache);
+  }
+
+  Future<String> loadSeachCache(String cache) async {
+    var prefs = await SharedPreferences.getInstance();
+    return prefs.getString('cache') ?? '';
+  }
 }
-
-final mapping = {
-  StorageKey.mainBalance: "mainBalance",
-  StorageKey.connectedWallets: "connectedWallets",
-  StorageKey.searchCache: "searchCache",
-  StorageKey.publicName: "publicName",
-};
-
-Future<String> loadValue(StorageKey key) async {}
